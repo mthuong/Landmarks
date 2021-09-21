4fc0327a2a788017a270d8dc09dc1a9ab8102ff5
Tom <thuong.nguyen@itads.net>
2021-09-21 15:53:17 +0700

    AppleWatch

---
+++ WatchLandmarks Extension/NotificationView.swift
@@ -0,0 +1,36 @@
+//
+//  NotificationView.swift
+//  WatchLandmarks Extension
+//
+//  Created by Thuong Nguyen on 9/21/21.
+//
+
+import SwiftUI
+
+struct NotificationView: View {
+    var title: String?
+    var message: String?
+    var landmark: Landmark?
+    
+    var body: some View {
+        VStack {
+            if landmark != nil {
+                CircleImage(image: landmark!.image.resizable())
+                    .scaledToFit()
+            }
+            
+            Text(title ?? "Unknown Landmark")
+                .font(.headline)
+            
+            Text(message ?? "You are within 5 miles of one of your favorite landmarks.")
+                .font(.caption)
+        }
+        .lineLimit(0)
+    }
+}
+
+struct NotificationView_Previews: PreviewProvider {
+    static var previews: some View {
+        NotificationView()
+    }
+}
