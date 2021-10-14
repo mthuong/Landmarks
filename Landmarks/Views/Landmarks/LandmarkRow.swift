4791f5204b857ca1e0985f0683e8dccc6cff596f
Tom <mttbit@gmail.com>
2021-10-14 22:47:18 +0700

    feat: finish swiftui tutorial

--- Landmarks/Views/Landmarks/LandmarkRow.swift
+++ Landmarks/Views/Landmarks/LandmarkRow.swift
@@ -15,7 +15,16 @@ struct LandmarkRow: View {
             landmark.image
                 .resizable()
                 .frame(width: 50, height: 50)
-            Text(landmark.name)
+                .cornerRadius(5.0)
+            VStack(alignment: .leading) {
+                Text(landmark.name)
+                    .bold()
+                #if !os(watchOS)
+                Text(landmark.park)
+                    .font(.caption)
+                    .foregroundColor(.secondary)
+                #endif
+            }
             
             Spacer()
             
@@ -24,12 +33,13 @@ struct LandmarkRow: View {
                     .foregroundColor(.yellow)
             }
         }
+        .padding(.vertical, 4)
     }
 }
 
 struct LandmarkRow_Previews: PreviewProvider {
     static var landmarks = ModelData().landmarks
-
+    
     static var previews: some View {
         Group {
             LandmarkRow(landmark: landmarks[0])
